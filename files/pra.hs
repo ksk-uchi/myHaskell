4ee86e1f8c1a83863020a685e62eced4192c1226
ksk-uchi <ksk.uchi+github@gmail.com>
2017-10-01 21:18:40 +0900

    Add practice

--- files/pra.hs
+++ files/pra.hs
@@ -29,8 +29,28 @@ makeStringUpper :: [String] -> [String]
 makeStringUpper xs = [[makeCharUpper (head x)] ++ (tail x) | x <- xs]
 
 fibonacci :: Integer -> [Integer]
-fibonacci 0 = [0]
-fibonacci 1 = [0, 1]
+fibonacci 0 = []
+fibonacci 1 = [0]
+fibonacci 2 = [0, 1]
 fibonacci x = 
     before ++ [sum (take 2 (reverse (before)))]
     where before = fibonacci (x - 1)
+
+fibonacci' :: Integer -> [Integer]
+fibonacci' 0 = []
+fibonacci' 1 = [0]
+fibonacci' 2 = [0, 1]
+fibonacci' x = 
+    let before = fibonacci' (x - 1)
+    in before ++ [sum (take 2 (reverse (before)))]
+
+addVector :: (Num a) => (a, a) -> (a, a) -> (a, a)
+addVector (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+head' :: [a] -> a
+head' [] = error "This is an Error"
+head' (x:_) = x
+
+head2' :: [a] -> a
+head2' xs = case xs of [] -> error "This is an Error"
+                       (x:_) -> x
